2f564a5bcb1efdca051f36a1bdae94203046e192
mnazarethg <mnazarethg@gmail.com>
2024-06-17 13:37:54 +0200

    const bcrypt = require('bcryptjs')

--- controllers/login.js
+++ controllers/login.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const router = require('express').Router()
 const User = require('../models/user')
 
